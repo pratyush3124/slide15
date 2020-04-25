81fc39ce79d31ec8f1c986c8f6998a75d73480a7
pratyush3124 <connect2pratyush@gmail.com>
2020-04-25 12:03:23 +0530

    aesthetics

--- slide15.py
+++ slide15.py
@@ -10,15 +10,17 @@ class Slide15(Tk):
 class game(Frame):
     def __init__(self,parent):
         Frame.__init__(self,parent)
+        font = ('Banschrift',25)
+        color = '#b2babb'
         self.llist = [[0]*4 for _ in range(4)]
         for i in range(4):
             for j in range(4):
                 if i != 3 or j!=3:
-                    self.llist[i][j] = Label(self,text=i*4+j+1,background = 'orange',bd = 1, relief = 'ridge',height = 4, width = 8)
+                    self.llist[i][j] = Label(self,text=i*4+j+1,font = font,background = color,bd = 1, relief = 'ridge',height = 2, width = 4)
                     self.llist[i][j].grid(row = i, column = j)
                     self.llist[i][j].bind('<Button-1>',lambda event, r = i, c = j: self.click(r,c))
                 else:
-                    self.llist[i][j] = Label(self,text=' ',background = 'white',bd = 1, relief = 'ridge',height = 4, width = 8)
+                    self.llist[i][j] = Label(self,text=' ',font = ('Banschrift',25),background = 'white',bd = 1, relief = 'ridge',height = 2, width = 4)
                     self.llist[i][j].grid(row = i, column = j)
                     self.llist[i][j].bind('<Button-1>',lambda event, r = i, c = j: self.click(r,c))
 
